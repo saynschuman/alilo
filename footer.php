d72161e4c78db5e78be821823fe006f8cad2b961
saynschuman <saynschuman@gmail.com>
2017-06-30 12:35:14 +0300

    menu fixed

--- footer.php
+++ footer.php
@@ -21,5 +21,71 @@
 		<!-- Show Google Analytics -->
 	<?php } ?>
 	<?php wp_footer(); ?> <!-- this is used by many Wordpress features and for plugins to work properly -->
+	<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
+	<style>
+		.icon-rss:before {
+		    content: "\f189";
+		}
+		.icon-google-plus:before {
+		    content: "\f16d";
+		}
+
+		.icon-pinterest:before {
+		    content: "\f263";
+		}
+		.logo {
+		    left: 50%;
+		    text-align: center;
+		}
+		.call-num {
+		    display: block;
+		    font-size: 18px;
+		    top: 18px;
+		    margin-bottom: 5px;
+		    text-align: right;
+		    position: absolute;
+		    right: 47px;
+		}		
+		.icon-twitter:before {
+		    content: "\f167";
+		}
+		header nav.fixed {
+				position: fixed;
+				background: #fff;
+				top: 0;
+				padding-bottom: 13px;
+		}
+		@media screen and (max-width: 769px) {
+			.logo {
+				left: 0;
+			}
+			.call-num {
+			    display: block;
+			    font-size: 18px;
+			    text-align: center;
+			    position: relative;
+			    right: 0;
+			    margin-bottom: 10px;
+			}
+			.social-nets-wrapper .social {
+				display: block;
+				text-align: center;
+			}
+		} 
+	</style>
+	<script>
+		(function($) {
+    		
+    		$(window).scroll(function(){
+    			if ($(this).scrollTop() > 100) {
+    				$('.nav__primary').addClass('fixed');
+    			} if ($(this).scrollTop() <= 100) {
+    				$('.nav__primary').removeClass('fixed');
+    			}
+        });
+
+	
+		})(jQuery);
+	</script>
 </body>
 </html>
